b6350612db8e1d4b310b8f16b4cafa64acd908dd
GuyHolman <44711773+GuyHolman@users.noreply.github.com>
2018-11-07 11:35:30 -0800

    w

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -12,7 +12,7 @@ public class Ship extends Token {
 
     public static final int MAX_SHIPS = 5;
 
-     public static Image Submarine = Toolkit.getDefaultToolkit().getImage("./assets/Submarine.jpg");
+    public static Image Submarine = Toolkit.getDefaultToolkit().getImage("./assets/Submarine.jpg");
     public static Image Cruiser = Toolkit.getDefaultToolkit().getImage("./assets/Cruiser.png");
     public static Image Scout = Toolkit.getDefaultToolkit().getImage("./assets/Scout.png");
     public static Image Destroyer = Toolkit.getDefaultToolkit().getImage("./assets/Destroyer.png");
